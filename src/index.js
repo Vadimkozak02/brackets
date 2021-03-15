4f7dc8b0a59f3a8b3efadaca50c9974252a2a39e
Vadim <vadimkozak02@gmail.com>
2021-03-15 21:18:59 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str) {
+  let stack = [];
+    let last;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == '[' || str[i] == '(' || str[i] == '{') { 
+            stack.push(str[i]);
+        } else if (str[i] == ']' || str[i] == ')' || str[i] == '}') { 
+            if (stack.length) { 
+                last = stack[stack.length - 1]; 
+                if ((last == '[' && str[i] == ']') || (last == '(' && str[i] == ')') ||
+                    (last == '{' && str[i] == '}')) {
+                    stack.pop(); 
+                }
+            } else if (stack != 0) {
+
+            } else return false;
+        } 
+    }return (!stack.length);
+};
